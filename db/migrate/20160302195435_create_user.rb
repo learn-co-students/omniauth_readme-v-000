b246bcc315730bb9f5f0ea5a69d9fcd36f6fc4ef
plai217 <philiplai217@gmail.com>
2016-03-02 15:00:58 -0500

    Done.

---
+++ db/migrate/20160302195435_create_user.rb
@@ -0,0 +1,9 @@
+class CreateUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.integer "uid", limit: 8
+    end
+  end
+end
