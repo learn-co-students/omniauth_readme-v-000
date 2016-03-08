3620a505b31235036004bd886fc4d9d46dafc321
Adam King <adam.king0126@gmail.com>
2016-03-08 05:55:31 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160308114938) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "info"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "uid"
+  end
 
 end
