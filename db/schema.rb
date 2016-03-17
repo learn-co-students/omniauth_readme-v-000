c7b263426dbfa0e2d9321f96bd69dbf5388b0621
Chris Aporta <christopher.aporta@gmail.com>
2016-03-16 22:57:05 -0400

    complete lab

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160317025151) do
+
+  create_table "users", force: :cascade do |t|
+    t.integer "uid"
+    t.string  "name"
+    t.string  "email"
+    t.string  "info"
+  end
 
 end
