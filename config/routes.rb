13df8608e8c99640b9ff1a0c1b3a02a2b427aa80
Joseph Surla <josephsurla@gmail.com>
2016-03-24 14:48:40 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-  
+  get 'sessions/create'
+
+  get '/auth/facebook/callback' => 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
