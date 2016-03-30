7ba79c75c6d98bdb06c82e48db19ecbdd9e39d07
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-03-30 22:47:00 +0000

    done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, ENV['1104646612933137'], ENV['●●●●●●●●']
+        provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
     end
