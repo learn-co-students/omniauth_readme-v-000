b1b5600593139723112f34698a5083ed2bac7779
authorbeard <msnolan@gmail.com>
2016-04-09 21:30:37 -0400

    testing gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   
+  get '/home', to: 'static#home', as: 'home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
