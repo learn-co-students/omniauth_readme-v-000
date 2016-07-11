65a0df941c446307204fef0ae0cea24cf9d9ca09
lawrend <douglaslawrence314@gmail.com>
2016-07-11 01:48:12 -0700

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def create
+  	user=User.create(user_params)
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email)
+  end
+end
