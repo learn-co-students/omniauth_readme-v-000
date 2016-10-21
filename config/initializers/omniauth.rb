15c5cf7bd6f0cbe25bcae22d4bc87f1df632dd0d
DownsideupmediaUSA <brandondecarlo@live.com>
2016-10-21 12:07:02 -0400

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use Omniauth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "176119336174250",
   "ee6a166233057f3ad7789b26c324e6c0"
 end
