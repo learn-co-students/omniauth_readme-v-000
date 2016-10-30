7b29c887833d0402e0efdb24a150fb46f24094ea
araf91 <araft50@yahoo.com>
2016-10-30 13:25:37 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
-  
+  resource :sessions
+  root "welcome#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
