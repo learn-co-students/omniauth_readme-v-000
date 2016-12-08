296460cbc001a96b59559024b931f84f34772f0e
Hilary Lyttle <hilyttle@gmail.com>
2016-12-08 21:15:43 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,9 @@
 ActiveRecord::Schema.define(version: 20161208204540) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.integer  "uid",        limit: 8
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.string  "name"
+    t.string  "email"
+    t.integer "uid",   limit: 8
   end
 
 end
