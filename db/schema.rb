707c34c245a7403843df15f891c3bdd144b170e2
Brad Watson <brad.watson.orlando@gmail.com>
2017-01-09 17:21:37 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170109221916) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "uid"
+  end
 
 end
