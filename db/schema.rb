462bfb96467053ad0313d7ac144e500c741bb671
John Pfingst <jpfingst@outlook.com>
2017-04-13 00:25:50 +0000

    omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170413002359) do
+
+  create_table "users", force: :cascade do |t|
+    t.integer  "uid",        limit: 8
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
 end
