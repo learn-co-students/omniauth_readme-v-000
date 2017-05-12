87ba75ac19f2401b6d7be11078f48bdd8ac564b0
Howard DeVennish <howarddevennish@gmail.com>
2017-05-12 21:06:28 +0000

    Done.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '1452917478100537', '07223dd30bff9b708fd5859a27601de5'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :sessions
+  root to: 'welcome#home'
 
   # Example resource route with options:
   #   resources :products do
