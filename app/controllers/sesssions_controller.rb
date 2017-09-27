0c0d52b740026f961a7c394a754c86c819b19423
Terry Blue <terryblue@comcast.net>
2017-09-27 20:20:30 +0000

    Done.

--- app/controllers/sesssions_controller.rb
+++ app/controllers/sesssions_controller.rb
@@ -6,14 +6,13 @@ class SessionsController < ApplicationController
       u.email = auth['info']['email']
       u.image = auth['info']['image']
     end
- 
     session[:user_id] = @user.id
- 
+
     render 'welcome/home'
   end
 
   private
- 
+
   def auth
     request.env['omniauth.auth']
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'welcome#home'
 
   get '/auth/facebook/callback' => 'sessions#create'
+  root 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
