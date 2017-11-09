715439d45f413684d6fd8c783d9fef221586cd37
Joanna <jmgsese@gmail.com>
2017-11-09 17:14:54 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
       u.image = auth['info']['image']
     end
 
-    binding.pry
-    
+    # binding.pry
+
     session[:user_id] = @user.id
 
     render 'welcome/home'
