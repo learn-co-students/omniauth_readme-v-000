761accf5722e1fb5ba47eb40e0e6c70757f987a3
charlie  lee <ctlee9096@gmail.com>
2017-11-25 17:59:01 +0000

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
   #ENV constant refers to a global hash for your entire computer environment
   #need two pieces of information from Facebook in order to
   #1) get authentication working:
