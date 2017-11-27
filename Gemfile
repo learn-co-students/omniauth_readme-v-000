ff396c767e658572471696c5fa4ea87272c7d9d4
ndalcin <dalcin.nicole@gmail.com>
2017-11-26 19:47:22 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'turbolinks'
 # gem 'capistrano-rails', group: :development
 
 gem 'omniauth'
-gem 'omniauth-facebook'
+gem 'omniauth-github'
 gem 'dotenv-rails'
 
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 class SessionsController < ApplicationController
 
 def create
-   @user = User.find_or_create_by(uid: auth['uid']) do |u|
-     u.name = auth['info']['name']
-     u.email = auth['info']['email']
-     u.image = auth['info']['image']
-   end
+  if auth_hash = request.env["omniauth.auth"]
+    #logged in via OmniAuth
+    oath_email = request.env["omniauth.auth"]["info"]["email"]
+    if user = User.find_by(:email => oath_email)
+      session[:user_id] = user.id
+    else
+      user = User.new(:email => oath_email)
 
-   binding.pry
-   
-   session[:user_id] = @user.id
-
+      session[:user_id] = user.id
+    end
+  end
+  "THIS WAS SUCCESSFUL?!"
    render 'welcome/home'
  end
 
