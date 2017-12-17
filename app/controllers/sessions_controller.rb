36e27c26ec1d07a3fd653e1800c481d8cfed0d3e
Sam Ojling <s.ojling@gmail.com>
2017-12-17 14:36:01 -0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsControllerController < ApplicationController
+class SessionsController < ApplicationController
 
   def create
     @user = User.find_or_create_by(uid: auth[:uid]) do |u|
