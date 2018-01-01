32eee9babca394d08624264ac023772d8cc46b87
Oski <github email address>
2018-01-01 18:17:45 -0500

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_url: "http://localhost:3000/auth/facebook/callback"
   end
---
+++ db/migrate/20180101230936_add_name_and_uid_to_users.rb
@@ -0,0 +1,12 @@
+class AddNameAndUidToUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :uid
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
