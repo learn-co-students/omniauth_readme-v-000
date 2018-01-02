188018b80a4e9e0cc824071b8ade5ec343a404d0
chrisjamesr <chrisjames.r@gmail.com>
2018-01-01 20:39:03 -0500

    fix type in initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-Rails.application.config.middleware.use
-OmniAuth::Builder do
-  provider :facebook, ENC['FACEBOOK_KEY'],
-  ENV['FACEBOOK_SECRET']
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
