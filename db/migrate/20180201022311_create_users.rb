b640b7b4d900bdf1936c7e38e40e9d5281ab1114
Justin Tooley <jstooley823@gmail.com>
2018-02-01 02:37:04 +0000

    Done.

---
+++ db/migrate/20180201022311_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    def change
+    create_table :products do |t|
+      t.string :name
+      t.string :email
+      t.string :image
+      t.string :uid
+
+      t.timestamps
+    end
+  end
+  end
+end
