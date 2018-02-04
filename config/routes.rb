1ba88b7f78213053ab91d32cc3ac514181af5217
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-04 20:37:14 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  get '/auth/facebook/callback' => 'sessions#create'
   root 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
