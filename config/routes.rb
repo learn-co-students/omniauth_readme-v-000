615f49dd1d69d1c28c10167abe6131627a9fe270
daniellecroxton <danikrist@gmail.com>
2018-03-23 22:05:21 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   root 'welcome#home'
+  # devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  get '/auth/google_oauth2/callback' => 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
