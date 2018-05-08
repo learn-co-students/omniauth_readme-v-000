4627245ffaa4f30ec873735cdb8ca00872fa2e35
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-05-08 11:59:34 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+	def create 
+
+		@user = User.find_or_create_by(uid: auth['uid']) 
+		@user.name = auth['info']['name']
+		@user.email = auth['info']['email']
+		@user.image = auth['info']['image']
+		
+		session[:user_id] = @user.id 
+
+		render 'welcome/home'
+	end 
+
+	private 
+
+	def auth
+		request.env['omniauth.auth']
+	end 
+
+end
