74250c8ad6183e86f4d252703c911ce8f422cc36
roitblatari <roitblatari@gmail.com>
2018-06-18 17:53:03 -0400

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,3 @@
-/Users/aronroitblat/Development/code/omniauth_readme-v-000/config/initializers/omniauth.rb
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+end
