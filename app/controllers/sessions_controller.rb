dece4391c3df16f2d1d9544418086d46ddfab591
github username <github email address>
2018-06-21 10:26:50 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
             u.image = auth['info']['image']
         end
 
+        binding.pry
        session[:user_id] = @user.id 
        
        render 'welcome/home'
