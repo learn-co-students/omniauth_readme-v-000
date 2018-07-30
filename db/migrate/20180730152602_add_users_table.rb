634d2c426a81f2faf947ce610bece7d561b82fc0
lauraBo <BodellLaura@gmail.com>
2018-07-30 11:37:56 -0400

    Done.

---
+++ db/migrate/20180730152602_add_users_table.rb
@@ -0,0 +1,12 @@
+class AddUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :image
+      t.string :uid
+
+      t.timestamps
+    end
+  end
+end
