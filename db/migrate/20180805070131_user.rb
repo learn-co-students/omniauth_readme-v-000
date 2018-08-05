ebc44944ff1101f7e86435b1263d08d22e2ea96d
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 07:35:04 +0000

    Done.

---
+++ db/migrate/20180805070131_user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :image
+      t.integer :uid
+    end
+  end
+
+end
