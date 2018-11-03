ae07ea56000a65890d3e3325b6a3eb888d685945
emanuelbierman <emanuel.bierman@gmail.com>
2018-11-03 09:55:49 -0400

    Done.

---
+++ db/migrate/20181102164635_add_omniauth_to_users.rb
@@ -0,0 +1,9 @@
+class AddOmniauthToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :token, :string
+    add_column :users, :expires_at, :integer
+    add_column :users, :expires, :boolean
+    add_column :users, :refresh_token, :string
+  end
+end
