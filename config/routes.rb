147bdfbbbab11fda0e79705e47c0ce410308b7aa
kev112 <werewaswas@live.com>
2018-11-07 14:03:07 -0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root 'welcome#home'
 
+  get '/auth/facebook/callback' => 'sessions#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+
+
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
