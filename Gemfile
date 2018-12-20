2fc660d2835364325d9005b8cc9b68879cfcb3a8
atxrenegade <harleighabel@gmail.com>
2018-12-20 01:37:53 -0600

    Done.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
+gem 'omniauth'
+gem 'omniauth-github'
+gem 'dotenv-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 4.2'
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get '/', to: "users#new"
+  get "/auth/github/callback", to: "sessions#create"
+  get "/signout", to: "sessions#destroy"
+
 end
