24f155f8841f175486c7cfe38b1b5375e0010245
atxrenegade <harleighabel@gmail.com>
2018-12-21 00:53:29 -0600

    Done.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 gem 'omniauth'
 gem 'omniauth-github'
 gem 'dotenv-rails'
+gem 'bcrypt'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 4.2'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,18 @@
 class SessionsController < ApplicationController
-
   def create
-	@user = User.find_or_create_by(uid: auth['uid']) do |u|
-		u.name = auth['info']['name']
-		u.email = auth['info']['email']
-		u.image = auth['info']['image']
-	end
-	session[:user_id] = @user.id
-	render 'welcome/home'
-end
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to '/welcome/home', notice: "Signed in!"
+  end
+
+  def destroy
+	session[:user_id] = nil
+	redirect_to root_url, notice: "Signed out!"
+  end
 
   private
 	def auth
 		request.env['ominauth.auth']
 	end
-  end		
-
-   #def create
-	#auth = request.env["omniauth.auth"]
-	#user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || #User.create_with_omniauth(auth)     #session[:user_id] = user.id     #redirect_to root_url, notice: "Signed in!"
- # end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out!"
-  end
 end
