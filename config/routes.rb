ace0f0380a6693dee59786fee852bf5839fc2fd1
github username <github email address>
2019-01-04 05:29:47 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
   root 'welcome#home'
+
+  get '/auth/facebook/callback' => 'sessions#create'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
