c2cf8194e2b40b3255c2f3e8f72e1646820d2a13
George Pianka <gpianka1@gmail.com>
2019-02-18 22:39:15 -0800

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module OmniauthLab
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # for Facebook required https
+    config.force_ssl = true
+    
+    
   end
 end
