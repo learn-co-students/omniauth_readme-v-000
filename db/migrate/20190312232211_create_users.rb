3416cd6fbf76089b1cd0441e76cb4992d6bfcc97
github username <github email address>
2019-03-14 14:23:49 -0700

    Done.

--- db/migrate/20190312232211_create_users.rb
+++ db/migrate/20190312232211_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string, :name
-      t.string, :email
-      t.string, :image
+      t.string :name
+      t.string :email
+      t.string :image
       t.string :uid
 
       t.timestamps null: false
