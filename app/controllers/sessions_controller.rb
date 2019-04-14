d511eafaeefe21f0f2c093a465b437ed89fb340e
RonSala <ron.sala@gmail.com>
2019-04-14 02:27:46 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
       u.email = auth['info']['email']
       u.image = auth['info']['image']
     end
+
+    binding.pry
  
     session[:user_id] = @user.id
  
