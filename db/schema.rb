3c6c870a04b04c754d0edbb8216fa0f66b1fe84c
Leslie Stevens <flymaven@protonmail.com>
2019-04-26 16:53:40 -0500

    add omniautho for github:

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190426202948) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
