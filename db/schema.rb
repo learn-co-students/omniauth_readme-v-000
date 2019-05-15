dc354ac5a19f1b2fcc2e42498e04c8d46ee291a5
ricecake88 <vocalise@gmail.com>
2019-05-14 22:57:54 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190514063942) do
+
+  create_table "users", force: :cascade do |t|
+    t.string  "name"
+    t.string  "email"
+    t.string  "image"
+    t.integer "uid",   limit: 8
+  end
 
 end
