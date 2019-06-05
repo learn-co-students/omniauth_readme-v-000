0b0b36642618c589b1cc7c22dab8957a389246f3
Heshie Brody <4hbrody@gmail.com>
2019-06-04 22:21:16 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 class SessionsController < ApplicationController
   def create
-    binding.pry
     @user = User.find_or_create_by(uid: auth[:uid]) do |u|
       u.name = auth['info']['name']
       u.uid = auth['uid']
       u.provider = auth['provider']
     end
 
-    session[:user_id] @user.id
+    session[:user_id] = @user.id
 
-    render: 'welcome/home'
+    render :'welcome/home'
   end
 
   private
