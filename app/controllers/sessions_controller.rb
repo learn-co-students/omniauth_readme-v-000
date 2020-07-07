7adce6e5df369f59bb39f8332980f6411e371a8b
Mitchell Beebe <beebe.mitch@gmail.com>
2020-07-07 14:12:37 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
     def create
-        @user = User.find_or_create_by(email: auth['email']) do |u|
+        # binding.pry
+        @user = User.find_or_create_by(email: auth['info']['email']) do |u|
+            u.name = auth['info']['name']
             u.email = auth['info']['email']
-            u.name = auth['info']['profile']
         end
    
       session[:user_id] = @user.id
